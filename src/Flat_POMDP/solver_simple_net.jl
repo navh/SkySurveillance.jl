9c8029744588d64005b457cfdb6dfe7722d1f788
navh <amos@amoshebb.com>
2023-12-09 13:52:34 -0500

    end-to-end working

--- src/Flat_POMDP/solver_simple_net.jl
+++ src/Flat_POMDP/solver_simple_net.jl
@@ -1,8 +1,11 @@
 struct SimpleGreedySolver <: Solver end
 
-N_TRAIN_EPISODES = 50000
+N_EPOCHS = 1
+N_TRAIN_EPISODES = 100
+N_TEST_EPISODES = 10
+N_SKIP_FIRST_STEPS = 100
 N_STEPS_PER_EPISODE = 100
-SKIP_FIRST_STEPS = 100
+MONTE_CARLO_ROLLOUTS = 10
 
 ACTION_WIDTH = 1
 BELIEF_WIDTH = 2 * PARAMS["number_of_targets"]
@@ -15,35 +18,91 @@ function POMDPs.solve(::SimpleGreedySolver, pomdp::POMDP)
 
     opt_state = Flux.setup(Adam(), model)
 
-    for _ in N_TRAIN_EPISODES
-        i = 0
-        child_pomdp = FlatPOMDP(rng, DISCOUNT)
-        updater = MultiFilterUpdater(child_pomdp.rng)
-        pomdp = BeliefPOMDP(child_pomdp.rng, child_pomdp, updater)
+    loss_history = []
+    for epoch_index in 1:N_EPOCHS
+        for _ in 1:N_TRAIN_EPISODES
+            i = 0
+            child_pomdp = FlatPOMDP(rng, DISCOUNT)
+            u = MultiFilterUpdater(child_pomdp.rng)
+            pomdp = BeliefPOMDP(child_pomdp.rng, child_pomdp, u)
 
-        for (a, o, r) in stepthrough(
-            pomdp,
-            RandomPolicy(pomdp.rng, pomdp, NothingUpdater()),
-            "a,o,r";
-            max_steps=N_STEPS_PER_EPISODE,
-        )
-            i += 1
-            if i > SKIP_FIRST_STEPS
-                belief_action = SVector{INPUT_WIDTH,Float32}(vcat(a, o))
-                grads = Flux.gradient(model) do m
-                    result = m(belief_action)
-                    mse(result, r)
+            for (a, o, r) in stepthrough(
+                pomdp,
+                RandomPolicy(pomdp.rng, pomdp, NothingUpdater()),
+                "a,o,r";
+                max_steps=N_SKIP_FIRST_STEPS + N_STEPS_PER_EPISODE,
+            )
+                i += 1
+                if i > N_SKIP_FIRST_STEPS
+                    grads = Flux.gradient(model) do m
+                        result = model(action_observation(a, o))
+                        mse(result, r)
+                    end
+                    Flux.update!(opt_state, model, grads[1])
                 end
-                Flux.update!(opt_state, model, grads[1])
             end
         end
+        sum_loss = 0.0
+        count_loss = 0
+        for _ in 1:N_TEST_EPISODES
+            i = 0
+            child_pomdp = FlatPOMDP(rng, DISCOUNT)
+            updater = MultiFilterUpdater(child_pomdp.rng)
+            pomdp = BeliefPOMDP(child_pomdp.rng, child_pomdp, updater)
+
+            for (a, o, r) in stepthrough(
+                pomdp,
+                RandomPolicy(pomdp.rng, pomdp, NothingUpdater()),
+                "a,o,r";
+                max_steps=N_SKIP_FIRST_STEPS + N_STEPS_PER_EPISODE,
+            )
+                i += 1
+                if i > N_SKIP_FIRST_STEPS
+                    result = model(action_observation(a, o))
+                    sum_loss += mse(result, r)
+                    count_loss += 1
+                end
+            end
+        end
+        mean_loss = sum_loss / count_loss
+        @info "epoch $(epoch_index) mean loss: $(mean_loss)"
+        push!(loss_history, mean_loss)
     end
 
-    # Next, train a model to predict score given belief and action
+    return MCTwigSPolicy(model)
+end
 
-    # This trained model is then used to create a policy that just 
-    # evaluates the current belief with a few possible actions, 
-    # and selects the best
+function action_observation(action, observation)
+    # return SVector{INPUT_WIDTH,Float32}(vcat(action, observation))
+    return Vector{Float32}(vcat(action, observation))
+end
 
-    return 42
+function monte_carlo_twig_search(model, observation, action_space)
+    best_action = rand(action_space)
+    best_estimate = model(action_observation(best_action, observation))
+    for a in rand(action_space, MONTE_CARLO_ROLLOUTS)
+        estimate = model(action_observation(a, observation))
+        if estimate > best_estimate
+            best_estimate = estimate
+            best_action = a
+        end
+    end
+    return best_action
+end
+
+struct MCTwigSPolicy{M<:Flux.Chain} <: Policy
+    model::M
+end
+
+function POMDPs.updater(::MCTwigSPolicy)
+    return PreviousObservationUpdater()
+end
+
+function POMDPs.action(p::MCTwigSPolicy, b::SVector)
+    # TODO: replace uniform(0,1) with some action_space(p.model) or something
+    return monte_carlo_twig_search(p.model, b, Uniform(0, 1))
+end
+
+function POMDPs.action(p::MCTwigSPolicy, b)
+    return 0.0
 end
--- src/SkySurveillance.jl
+++ src/SkySurveillance.jl
@@ -7,16 +7,16 @@ using Distributions: Normal, Uniform
 using Flux
 using Flux: glorot_uniform, mse
 using IntervalSets
-using POMDPTools.BeliefUpdaters: NothingUpdater
+using POMDPTools.BeliefUpdaters: NothingUpdater, PreviousObservationUpdater
 using POMDPTools.Simulators: Sim, run_parallel, stepthrough
 using POMDPTools:
     Deterministic, HistoryRecorder, POMDPTools, RandomPolicy, RandomSolver, eachstep
-using POMDPs: POMDP, POMDPs, Solver, Updater, discount, isterminal, reward, simulate, solve
+using POMDPs:
+    POMDP, POMDPs, Policy, Solver, Updater, discount, isterminal, reward, simulate, solve
 using Plots: @animate, Plots, RGB, Shape, distinguishable_colors, mov, plot, plot!
-using Random: AbstractRNG, Xoshiro
+using Random: AbstractRNG, Xoshiro, default_rng
 using StaticArrays: SVector
 using Statistics: mean, var
-using Random: default_rng
 using TOML: parse, parsefile
 
 if isempty(ARGS)
@@ -55,32 +55,32 @@ mkpath(PARAMS["log_path"])
 
 rng = Xoshiro(PARAMS["seed"])
 child_pomdp = FlatPOMDP(rng, DISCOUNT)
-updater = MultiFilterUpdater(child_pomdp.rng)
-pomdp = BeliefPOMDP(child_pomdp.rng, child_pomdp, updater)
+u = MultiFilterUpdater(child_pomdp.rng)
+pomdp = BeliefPOMDP(child_pomdp.rng, child_pomdp, u)
 #solver = RandomMultiFilter()
 #solver = RandomSolver()
 solver = SimpleGreedySolver()
 policy = solve(solver, pomdp)
-#
-# hr = HistoryRecorder(; max_steps=PARAMS["animation_steps"])
-# history = simulate(hr, pomdp, policy)
-#
-# if PARAMS["render"]
-#     anim = @animate for step in eachstep(history)
-#         POMDPTools.render(pomdp, step)
-#     end
-#     mkpath(PARAMS["video_path"])
-#     mov(anim, "$(PARAMS["video_path"])$(run_time).mov"; loop=1)
-# end
-#
-# rewards = [reward(pomdp, step.s, step.b) for step in eachstep(history)]
-#
-# mkpath(PARAMS["log_path"])
-# open("$(PARAMS["log_path"])$(run_time).txt", "w") do f
-#     for i in rewards
-#         println(f, i)
-#     end
-# end
+
+hr = HistoryRecorder(; max_steps=PARAMS["animation_steps"])
+history = simulate(hr, pomdp, policy)
+
+if PARAMS["render"]
+    anim = @animate for step in eachstep(history)
+        POMDPTools.render(pomdp, step)
+    end
+    mkpath(PARAMS["video_path"])
+    mov(anim, "$(PARAMS["video_path"])$(run_time).mov"; loop=1)
+end
+
+rewards = [reward(pomdp, step.s, step.b) for step in eachstep(history)]
+
+mkpath(PARAMS["log_path"])
+open("$(PARAMS["log_path"])$(run_time).txt", "w") do f
+    for i in rewards
+        println(f, i)
+    end
+end
 
 #### Crux.jl experiment begin
 #
